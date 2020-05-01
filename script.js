55409e63bc6f4c8f0df76dcd5fb494768ef60f43
Eric Deck <47580517+deckeric@users.noreply.github.com>
2020-05-01 11:17:41 -0500

    First commit.
    First commit. The layout for the game. Still needs work.

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+init();
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    // document.querySelector('.btn-new').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
